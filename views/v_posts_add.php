7be604497a2930cbe856607c52bde4f099db6341
Kirsten Ullrich <kirstenullrich@gmail.com>
2013-11-06 18:47:36 -0500

    yes, I watch class live

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -1,10 +1,12 @@
  <div class="contentwrap clearfix">
-
     <form class="bleatform" action="/posts/p_add" method="POST" id="bleatform">
         <label for="content">Bleat it out!</label>
         <textarea maxlength="150" autofocus="autofocus" name="content">
         </textarea>
         <input type="submit" value="Bleat">
     </form>
+
+     <p class="center bottommsg">Maximum 150 characters<br>"Slightly better than Twitter"</p>
+
 
 </div>
